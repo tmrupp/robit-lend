79d3c2fb68b3dab9313f32dcd18ceaa9cdf0f4e8
BSDunham <ben.dunham95@gmail.com>
2015-05-24 18:58:48 -0400

    Robitman

---
+++ 2D Practice/Assets/HurtRobit.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HurtRobit : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		Debug.Log ("BOOM");
+	}
+
+}
---
+++ 2D Practice/Assets/robitMove.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class robitMove : MonoBehaviour {
+	public float speed = .5f;
+
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+
+		Vector3 move = new Vector3 (h, v, 0.0f);
+		transform.position = transform.position + move * speed * Time.deltaTime;
+	}
+}
