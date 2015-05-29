d0d0c7e5d70db242c5bfec3ffc2f2e834535211f
tmrupp <tmrupp@umich.edu>
2015-05-28 21:58:06 -0400

    Fixed some things I messed up

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+	NavMeshAgent agent;
+	
+	void Start() {
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) 
+		{
+			RaycastHit hit;
+			
+			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
+				agent.destination = hit.point;
+			}
+		}
+	}
+}
